79c898918d4e50b5d3a3374285665808b17198ae
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-06-10 20:42:31 +0500

    Node-tasks

---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+// Set up token middleware
+const jwt = require('jsonwebtoken');
+const config = require("../config");
+
+module.exports =function  auth  (req, res, next){
+    // Check if the request contains a token and verify it
+    const token = req.header("x-auth-token");
+    if (!token) return res.status(401).send("Access denied. No token provided.");
+    try {
+      const decoded = jwt.verify(token, config.get("jwt_secret"));
+      req.user = decoded;
+      next();
+    } catch (ex) {
+      res.status(400).send("Invalid token.");
+    }
+  };
